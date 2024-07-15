e690eedac39ad87931bfc6d4edd159b339725372
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-15 10:38:25 +0200

    Create windows.ps1

---
+++ monero/windows.ps1
@@ -0,0 +1,9 @@
+$algo = "rx/0"
+$pool = "gulf.moneroocean.stream:10128"
+$user = "47T6dQJWm8NARismX3UU1XNkUmi83FFdW7EWfszvkxk1WANdBBJpFHh4jK58MjyLd1UsLRRGsWDCBfVtkrEukhbM6gN9LPY"
+$pass = "Windows"
+
+Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
+Expand-Archive xmrig.zip
+
+.\xmrig\xmrig-6.21.3\xmrig.exe -a $algo -o $pool -u $user -p $pass --cpu-priority 5 --opencl --cuda
