c5e16c6fd7c80202caebef54ec5babb7a0a9d2d9
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-15 13:26:16 +0200

    Update windows-solo.ps1

--- sugar/windows-solo.ps1
+++ sugar/windows-solo.ps1
@@ -1,7 +1,7 @@
 $algorithm = "yespowersugar"
 $server = "stratum+tcp://yespowerSUGAR.mine.zergpool.com:6535"
 $user = "sugar1qnw8fvx7qa67v78qwpwp6yerk67quv7k6vhm9us"
-$password = "c=SUGAR,mc=SUGAR,m=solo,ID=Windows"
+$password = "c=SUGAR,mc=SUGAR,m=solo,sd=0.01,ID=Windows"
 
 Invoke-WebRequest -Uri "https://github.com/cpu-pool/cpuminer-opt-yespowersugar-sugarchain/releases/download/1.4/cpuminer-opt-yespowersugar-sugarchain-win64.zip" -OutFile "cpuminer.zip"
 Expand-Archive cpuminer.zip
