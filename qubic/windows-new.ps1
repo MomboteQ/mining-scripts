d0bb7a594dfcd3e44c5791e12b0a8bb6a2fb5bb8
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-18 22:25:45 +0200

    Update and rename windows.ps1 to windows-new.ps1

--- qubic/windows-new.ps1
+++ qubic/windows-new.ps1
@@ -3,4 +3,4 @@ $wallet = "TMTKFLCERQTXMDWNEJZGBCQDJARCSKKWSJTKFBOZTBOZXRHXFQMZLQMAELSB"
 
 Invoke-WebRequest -Uri "https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.3.15/rqiner-x86.exe" -OutFile "rqiner.exe"
 
-rqiner.exe -i $wallet -t $threads
+.\rqiner.exe -i $wallet -t $threads
