d2409ccd5e2d0ca08e28cc98d68235e854731b58
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-24 12:26:41 +0200

    Update windows.ps1

--- epiccash/windows.ps1
+++ epiccash/windows.ps1
@@ -1,8 +1,9 @@
 $algo = "randomepic"
 $pool = "stratum+tcp://51pool.online:4416"
 $user = "momboteq#windows"
+$pass = "x"
 
 Invoke-WebRequest -Uri "https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.9/SRBMiner-Multi-2-5-9-win64.zip" -OutFile "srbminer.zip"
 Expand-Archive srbminer.zip
 
-.\srbminer\SRBMiner-Multi-2-5-9\SRBMiner-MULTI.exe -a $algo -o $pool -u $user
+.\srbminer\SRBMiner-Multi-2-5-9\SRBMiner-MULTI.exe -a $algo -o $pool -u $user -p $pass
