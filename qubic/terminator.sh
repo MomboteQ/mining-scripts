95d39956ef1900b433634d7fdb34d30927f2456c
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-06 20:32:31 +0100

    Update terminator.sh

--- qubic/terminator.sh
+++ qubic/terminator.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.3/rqiner-x86-znver2
 chmod +x rqiner-x86-znver2
-./rqiner-x86-znver2 -i TMTKFLCERQTXMDWNEJZGBCQDJARCSKKWSJTKFBOZTBOZXRHXFQMZLQMAELSB -t $(nproc)
+./rqiner-x86-znver2 -i TMTKFLCERQTXMDWNEJZGBCQDJARCSKKWSJTKFBOZTBOZXRHXFQMZLQMAELSB -t $(nproc) --label terminator
