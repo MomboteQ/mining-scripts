d94309b9cb323e09e6924e673ef05573f33be09f
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-06 21:46:17 +0100

    Update windows.ps1

--- monero/windows.ps1
+++ monero/windows.ps1
@@ -4,7 +4,7 @@ $user = "45MBTQ27xo1LJmAi6sNFTyZhgTdoF45XCRsLUtmL7Lo376nNuxcaSxAa3JUkUV4xHEVSpsE
 $pass = "Windows"
 $threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 
-Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
+Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
 
-.\xmrig\xmrig-6.21.3\xmrig.exe -a $algo -o $pool -u $user -p $pass --cpu-priority 5 -t $threads
+.\xmrig\xmrig-6.22.2\xmrig.exe -a $algo -o $pool -u $user -p $pass --cpu-priority 5 -t $threads
