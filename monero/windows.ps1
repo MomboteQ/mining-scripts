54d476b7c33fc207c8364d3c8e16dc2668a4b460
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-06 21:41:02 +0100

    Update windows.ps1

--- monero/windows.ps1
+++ monero/windows.ps1
@@ -1,9 +1,10 @@
 $algo = "rx/0"
 $pool = "gulf.moneroocean.stream:10001"
-$user = "47T6dQJWm8NARismX3UU1XNkUmi83FFdW7EWfszvkxk1WANdBBJpFHh4jK58MjyLd1UsLRRGsWDCBfVtkrEukhbM6gN9LPY"
+$user = "45MBTQ27xo1LJmAi6sNFTyZhgTdoF45XCRsLUtmL7Lo376nNuxcaSxAa3JUkUV4xHEVSpsEA6nwL5S28g1fCa1RwPfXpwnH"
 $pass = "Windows"
+$threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
 
-.\xmrig\xmrig-6.21.3\xmrig.exe -a $algo -o $pool -u $user -p $pass --cpu-priority 5
+.\xmrig\xmrig-6.21.3\xmrig.exe -a $algo -o $pool -u $user -p $pass --cpu-priority 5 -t $threads
