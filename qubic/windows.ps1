870a96b3faef23da1ad30775a656565976525754
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-07 12:47:16 +0100

    Update windows.ps1

--- qubic/windows.ps1
+++ qubic/windows.ps1
@@ -3,4 +3,4 @@ $threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessor
 Invoke-WebRequest -Uri "https://github.com/apool-io/apoolminer/releases/download/v2.8.2/apoolminer_win_autoupdate_v2.8.2.zip" -OutFile "apool.zip"
 Expand-Archive apool.zip
 
-.\apool\apoolminer_win_autoupdate_v2.8.2\apoolminer_win_autoupdate_v2.8.2\apoolminer.exe --algo qubic --account CP_0a38x1rh9e --pool qubic1.hk.apool.io:3334 --mode 1
+.\apool\apoolminer_win_autoupdate_v2.8.2\apoolminer_win_autoupdate_v2.8.2\apoolminer.exe --algo qubic --account CP_0a38x1rh9e --pool qubic1.hk.apool.io:3334 --mode 1 -t $threads
